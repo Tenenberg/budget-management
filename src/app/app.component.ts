4f5aba749701c2b027c016384d4e9ea80e6324aa
TAL <tenenberg@gmail.com>
2022-12-24 14:31:18 +0200

    remove modules init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
+  standalone: true,
   selector: 'budget-management-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  providers: [],
+  imports: [NxWelcomeComponent],
 })
 export class AppComponent {
   title = 'budget-management';
--- src/app/nx-welcome.component.ts
+++ src/app/nx-welcome.component.ts
@@ -3,6 +3,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
 /* eslint-disable */
 
 @Component({
+  standalone: true,
   selector: 'budget-management-nx-welcome',
   template: `
     <!--
